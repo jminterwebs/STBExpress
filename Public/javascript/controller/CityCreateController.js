b55851d9cfc0c9fa6de5ea84579474ed6dc3f4d9
jminterwebs <jmverdone@gmail.com>
2015-08-23 17:16:09 -0400

    Final

--- Public/javascript/controller/CityCreateController.js
+++ Public/javascript/controller/CityCreateController.js
@@ -4,27 +4,38 @@ angular.module('Cityapp').controller('CityCreateController', function(City, $sco
 
    // $scope.deleteNote = function(city){confirm(city.city.city);};
    
+   $scope.deleteNote = function(city){
 
-     $scope.deleteNote = function(city){
+  // Make the request
+  $http({
+    method: 'DELETE',
+    url: '/city',
+    data: city.city,
+    headers:{"Content-Type": "application/json;charset=utf-8"} 
+  })
+  // On success:
+  .then(function (){
+    var index = $scope.cities.indexOf(city.city);
+    var cityindex = city.city;
+    $scope.cities.splice(index, 1);
+  })
+  // On error:
+  .catch(function (){
+    // Do something better than this:
+    alert("Something bad happened");
+  })
+
+  .finally(function (){
+    // Re-enable the button.
+    city.deleting = false;
+  })
+
+  // Disable the delete button and show a loading animation based on
+  // this value (use `ng-disabled`).
+  city.deleting = true;
 
 
-         $http({
-        method: 'DELETE',
-        url: '/city',
-        data: city.city,
-        headers:{"Content-Type": "application/json;charset=utf-8"} });
-
-
-         var index = city.city;
-
-
-         $scope.cities.splice(index, 1);
-
-
-
-    };
-
-        
+};     
     $scope.saveCity = function(city){
     $http({
         method: 'POST',
--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var CityDb = require('./Server/Schema/CitySchema');
 var cityController = require('./Server/controllers/city-controllers');
 var port = process.env.PORT || 3333;
 
-var mongoPro = "mongolabconnectionhere";
+var mongoPro = "mongodb://heroku_rkzx8bcz:j7qq3peas9tgt5dds2oqd8dsg@ds035583.mongolab.com:35583/heroku_rkzx8bcz";
 var mongoDev = 'mongodb://localhost:27017/citydb';
-mongoose.connect( mongoDev);
+mongoose.connect( mongoPro);
 
 app.use(express.static(__dirname +'/Public'));
 app.use(bodyparser.json());
@@ -26,23 +26,30 @@ var routeCity = function(req, res) {
     };
 
 app.get('/city/:city', function(req,res){
-     CityDb.find({city: req.params.city}, function(err, results) {
+     CityDb.find({city: req.body.city}, function(err, results) {
             res.send(results);
             console.log(results);
-            console.log(typeof(results));
-            console.log(req.params.city);
+           
         });
 
 });
 
 app.delete('/city', function(req,res){
 
-    CityDb.findOneAndRemove({city: req.body.city}, function(err, results){
-        if (err) throw err;
+  CityDb.findOneAndRemove({city: req.body.city}, function(err, results){
 
-    });
+    if (err){
+      res.status(500).send({error: err});
+      // Assume you are going to catch this somewhere...
+      throw err;
+    }
 
+    else
+      res.status(200).send();
 
+  });
+
+    
 });
 
 app.listen(port, function(){
