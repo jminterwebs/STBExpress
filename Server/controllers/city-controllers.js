2cef2d84cf1b2241fd54b4b2a6f469de066f931d
John <jmverdone@gmail.com>
2015-08-23 20:10:19 -0400

    clean up

--- Server/controllers/city-controllers.js
+++ Server/controllers/city-controllers.js
@@ -2,55 +2,56 @@ var CityDB = require('../Schema/CitySchema');
 
 
 
-    module.exports.create = function(req,res){
+    // Save city to database
+  module.exports.create = function(req,res){
 
-       var cityDb = new CityDB(req.body);
+    var cityDb = new CityDB(req.body);
+    var newCity = req.body;
 
-        var newCity = req.body;
- if(!newCity.city || !newCity.desc){
-    res.sendStatus(400);
-
- } else{
-    cityDb.save (
-            CityDB.find({}, function(err,results) {
+     if(!newCity.city || !newCity.desc){
+        res.sendStatus(400);
+     }
+     else{
+            cityDb.save (CityDB.find({}, function(err,results) {
                 res.json(results);
             })
-        );
+            );
+    };
  };
 
-    };
 
-    module.exports.list = function(req, res) {
-      CityDB.find({}, function(err, results) {
-            res.json(results);
 
+    // Lists cities
+  module.exports.list = function(req, res) {
+
+     CityDB.find({}, function(err, results) {
+                 res.json(results);
         });
     };
 
-    module.exports.delete = function(req,res){
+    // Deletes cities on click of red X
+  module.exports.delete = function(req,res){
 
-  CityDB.findOneAndRemove({city: req.body.city}, function(err, results){
+    CityDB.findOneAndRemove({city: req.body.city}, function(err, results){
 
-    if (err){
-      res.status(500).send({error: err});
-      // Assume you are going to catch this somewhere...
-      throw err;
-    }
+        if (err){
+            res.status(500).send({error: err});
+            throw err;
+        }
+        else
+            res.status(200).send();
 
-    else
-      res.status(200).send();
+    });
 
-  });
+  };
 
-};
+    // Routes to page to display city and desc
+  module.exports.cityDesc =  function(req,res){
 
-    module.exports.cityDesc =  function(req,res){
-     CityDB.find({city: req.params.city}, function(err, results) {
+      CityDB.find({city: req.params.city}, function(err, results) {
+
             res.send(results);
-            console.log(results);
-
-        });
-
-    };
+      });
+  };
 
 
